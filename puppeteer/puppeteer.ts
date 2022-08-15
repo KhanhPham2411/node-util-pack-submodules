76484f366e1fbd8d8a013dbb8ca5db3a12d3cf5c
khanhpham-nano <kpham@nanolos.com>
2022-08-16 00:01:09 +0700

    ok

--- puppeteer/puppeteer.ts
+++ puppeteer/puppeteer.ts
@@ -11,21 +11,21 @@ import StealthPlugin from 'puppeteer-extra-plugin-stealth'
 // Spin up a browser in the background
 export let browser: Browser;
 
-export const setupBrowser = async () => {
+export const setupBrowser = async ({headless=true}) => {
   puppeteer
     .use(AdblockerPlugin())
     .use(StealthPlugin())
 
   browser = await puppeteer
-    .launch({ headless: true
+    .launch({ headless
       //, ignoreHTTPSErrors: true, defaultViewport: null
 			,executablePath: 'C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe'
       ,userDataDir: 'C:\\Users\\phamd\\AppData\\Local\\Google\\Chrome\\User Data2\\Test'
-      // args:[
-			// 	'--start-maximized',
-			// 	'--user-data-dir=C:\\Users\\phamd\\AppData\\Local\\Google\\Chrome\\User Data2',
-      //   '--profile-directory=Huong'
-			// ]
+      ,args:[
+				'--start-maximized',
+				// '--user-data-dir=C:\\Users\\phamd\\AppData\\Local\\Google\\Chrome\\User Data2',
+        // '--profile-directory=Huong'
+			]
     })
     
   // browser.on('disconnected', setupBrowser);
